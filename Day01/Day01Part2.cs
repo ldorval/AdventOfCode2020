2274fcf94989974c3d933ec09e15299e0593a6c9
Luc Dorval <ldorval@gmail.com>
2020-12-01 14:33:42 -0500

    Day 1 part 2

---
+++ Day01/Day01Part2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day01
+{
+    public class Day01Part2
+    {
+        public static int Solve(IList<int> expenses)
+        {
+            foreach (var expense1 in expenses)
+            {
+                foreach (var expense2 in expenses)
+                {
+                    foreach (var expense3 in expenses)
+                    {
+                        if (expense1 + expense2 + expense3 == 2020)
+                            return expense1 * expense2 * expense3;                        
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
