eb33fc97c91a0a70d8cc7df69aacfce4d643a454
Luc Dorval <ldorval@gmail.com>
2020-12-01 14:26:23 -0500

    Created project for advent of code 2020 with day 1 part 1 solution

---
+++ Day01/Day01Part1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day01
+{
+    public class Day01Part1
+    {
+        public static int Solve(IList<int> expenses)
+        {
+            foreach (var expense1 in expenses)
+            {
+                foreach (var expense2 in expenses)
+                {
+                    if (expense1 + expense2 == 2020)
+                        return expense1 * expense2;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
