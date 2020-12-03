28ba42fee7533bb869be2e479d9047b417acc0e5
Luc Dorval <ldorval@gmail.com>
2020-12-03 09:20:18 -0500

    Day 3 part 1 and 2

---
+++ Day03/Day03.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace AdventOfCode2020.Day03
+{
+    public class Day03
+    {
+        public static long Solve(List<string> map, int xDisplacement, int yDisplacement)
+        {
+            var coordinate = new Point(0, 0);
+            var treesCrossed = 0;
+
+            while (coordinate.Y < map.Count)
+            {
+                if (map[coordinate.Y][coordinate.X] == '#')
+                    treesCrossed++;
+                
+                coordinate.X = CalculateX(coordinate.X, map[coordinate.Y].Length, xDisplacement);
+                coordinate.Y += yDisplacement;
+            } 
+
+            return treesCrossed;
+        }
+
+        private static int CalculateX(int x, int lineLength, int xDisplacement)
+        {
+            var newX = x + xDisplacement;
+            return newX >= lineLength ? newX - lineLength : newX;
+        }
+    }
+}
---
+++ Day03/Day03Test.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2020.Day03
+{
+    public class Day03Test
+    {
+        private List<string> mapExample = new List<string>
+        {
+            "..##.......",
+            "#...#...#..",
+            ".#....#..#.",
+            "..#.#...#.#",
+            ".#...##..#.",
+            "..#.##.....",
+            ".#.#.#....#",
+            ".#........#",
+            "#.##...#...",
+            "#...##....#",
+            ".#..#...#.#"
+        };
+
+        [Test]
+        public void ExamplePart1()
+        {
+            Day03.Solve(mapExample, 3, 1).Should().Be(7);
+        }
+
+        [Test]
+        public void Solution1()
+        {
+            var map = InputReader.ReadLines("Day03/input.txt");
+            Console.WriteLine(Day03.Solve(map, 3, 1));
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var result = Day03.Solve(mapExample, 1, 1) *
+                         Day03.Solve(mapExample, 3, 1) *
+                         Day03.Solve(mapExample, 5, 1) *
+                         Day03.Solve(mapExample, 7, 1) *
+                         Day03.Solve(mapExample, 1, 2);
+
+            result.Should().Be(336);
+        }
+
+        [Test]
+        public void Solution2()
+        {
+            var map = InputReader.ReadLines("Day03/input.txt");
+            
+            var result = Day03.Solve(map, 1, 1) *
+                         Day03.Solve(map, 3, 1) *
+                         Day03.Solve(map, 5, 1) *
+                         Day03.Solve(map, 7, 1) *
+                         Day03.Solve(map, 1, 2);
+            
+            Console.WriteLine(result);
+        }
+    }
+}
