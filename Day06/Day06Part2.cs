30591cb5b9064485b64b31c8158c9d4b551b67d8
Luc Dorval <ldorval@gmail.com>
2020-12-06 12:31:44 -0500

    Change day 6 part 2 code to be fully LINQ

--- Day06/Day06Part2.cs
+++ Day06/Day06Part2.cs
@@ -1,27 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020.Day06
 {
     public class Day06Part2
     {
+        private static readonly IList<char> Alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
         public static int Solve(string input)
         {
             return input
                 .Split($"{Environment.NewLine}{Environment.NewLine}")
                 .Select(x => x.Split(Environment.NewLine))
-                .Sum(group =>
-                {
-                    var count = 0;
-                    
-                    for (var c = 'a'; c <= 'z'; c++)
-                    {
-                        if (group.All(x => x.Contains(c)))
-                            count++;
-                    }
-
-                    return count;
-                });
+                .Sum(group => Alphabet.Count(a => group.All(g => g.Contains(a))));
         }
     }
 }
